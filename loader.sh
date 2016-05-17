d273bc13f92eae98135c300b62e3521ca0dcead5
codeapps <codeapps@aol.com>
2016-05-17 16:20:45 -0500

    Loader

---
+++ loader.sh
@@ -0,0 +1,19 @@
+qlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/articulo.ctl data=/home/oracle/database/Oracle/csv/articulo.csv log=/home/oracle/database/Oracle/log/articulo.log errors=3000
+
+sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/vendedor.ctl data=/home/oracle/database/Oracle/csv/vendedor.csv log=/home/oracle/database/Oracle/log/vendedor.log errors=3000
+
+sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/cliente.ctl data=/home/oracle/database/Oracle/csv/cliente.csv log=/home/oracle/database/Oracle/log/cliente.log errors=3000
+
+sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/conductor.ctl data=/home/oracle/database/Oracle/csv/conductor.csv log=/home/oracle/database/Oracle/log/conductor.log errors=3000
+
+sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/ayudante.ctl data=/home/oracle/database/Oracle/csv/ayudante.csv log=/home/oracle/database/Oracle/log/ayudante.log errors=3000
+
+sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/vehiculo.ctl data=/home/oracle/database/Oracle/csv/vehiculo.csv log=/home/oracle/database/Oracle/log/vehiculo.log errors=3000
+
+sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/cabecera.ctl data=/home/oracle/database/Oracle/csv/cabecera.csv log=/home/oracle/database/Oracle/log/cabecera.log errors=3000
+
+sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/detalle.ctl data=/home/oracle/database/Oracle/csv/detalle.csv log=/home/oracle/database/Oracle/log/detalle.log errors=3000
+
+sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/carguec.ctl data=/home/oracle/database/Oracle/csv/carguec.csv log=/home/oracle/database/Oracle/log/carguec.log errors=3000
+
+sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/cargued.ctl data=/home/oracle/database/Oracle/csv/cargued.csv log=/home/oracle/database/Oracle/log/cargued.log errors=3000
