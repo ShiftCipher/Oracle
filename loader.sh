3039edfaca928df0f77a8c778ad3e4e20469604a
codeapps <codeapps@aol.com>
2016-05-17 16:33:09 -0500

    Oracle -> oracle

--- loader.sh
+++ loader.sh
@@ -1,19 +1,19 @@
-qlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/articulo.ctl data=/home/oracle/database/Oracle/csv/articulo.csv log=/home/oracle/database/Oracle/log/articulo.log errors=3000
+qlldr userid=daniel/123456 control=/home/oracle/database/oracle/ctl/articulo.ctl data=/home/oracle/database/oracle/csv/articulo.csv log=/home/oracle/database/oracle/log/articulo.log errors=3000
 
-sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/vendedor.ctl data=/home/oracle/database/Oracle/csv/vendedor.csv log=/home/oracle/database/Oracle/log/vendedor.log errors=3000
+sqlldr userid=daniel/123456 control=/home/oracle/database/oracle/ctl/vendedor.ctl data=/home/oracle/database/oracle/csv/vendedor.csv log=/home/oracle/database/oracle/log/vendedor.log errors=3000
 
-sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/cliente.ctl data=/home/oracle/database/Oracle/csv/cliente.csv log=/home/oracle/database/Oracle/log/cliente.log errors=3000
+sqlldr userid=daniel/123456 control=/home/oracle/database/oracle/ctl/cliente.ctl data=/home/oracle/database/oracle/csv/cliente.csv log=/home/oracle/database/oracle/log/cliente.log errors=3000
 
-sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/conductor.ctl data=/home/oracle/database/Oracle/csv/conductor.csv log=/home/oracle/database/Oracle/log/conductor.log errors=3000
+sqlldr userid=daniel/123456 control=/home/oracle/database/oracle/ctl/conductor.ctl data=/home/oracle/database/oracle/csv/conductor.csv log=/home/oracle/database/oracle/log/conductor.log errors=3000
 
-sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/ayudante.ctl data=/home/oracle/database/Oracle/csv/ayudante.csv log=/home/oracle/database/Oracle/log/ayudante.log errors=3000
+sqlldr userid=daniel/123456 control=/home/oracle/database/oracle/ctl/ayudante.ctl data=/home/oracle/database/oracle/csv/ayudante.csv log=/home/oracle/database/oracle/log/ayudante.log errors=3000
 
-sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/vehiculo.ctl data=/home/oracle/database/Oracle/csv/vehiculo.csv log=/home/oracle/database/Oracle/log/vehiculo.log errors=3000
+sqlldr userid=daniel/123456 control=/home/oracle/database/oracle/ctl/vehiculo.ctl data=/home/oracle/database/oracle/csv/vehiculo.csv log=/home/oracle/database/oracle/log/vehiculo.log errors=3000
 
-sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/cabecera.ctl data=/home/oracle/database/Oracle/csv/cabecera.csv log=/home/oracle/database/Oracle/log/cabecera.log errors=3000
+sqlldr userid=daniel/123456 control=/home/oracle/database/oracle/ctl/cabecera.ctl data=/home/oracle/database/oracle/csv/cabecera.csv log=/home/oracle/database/oracle/log/cabecera.log errors=3000
 
-sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/detalle.ctl data=/home/oracle/database/Oracle/csv/detalle.csv log=/home/oracle/database/Oracle/log/detalle.log errors=3000
+sqlldr userid=daniel/123456 control=/home/oracle/database/oracle/ctl/detalle.ctl data=/home/oracle/database/oracle/csv/detalle.csv log=/home/oracle/database/oracle/log/detalle.log errors=3000
 
-sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/carguec.ctl data=/home/oracle/database/Oracle/csv/carguec.csv log=/home/oracle/database/Oracle/log/carguec.log errors=3000
+sqlldr userid=daniel/123456 control=/home/oracle/database/oracle/ctl/carguec.ctl data=/home/oracle/database/oracle/csv/carguec.csv log=/home/oracle/database/oracle/log/carguec.log errors=3000
 
-sqlldr userid=daniel/123456 control=/home/oracle/database/Oracle/ctl/cargued.ctl data=/home/oracle/database/Oracle/csv/cargued.csv log=/home/oracle/database/Oracle/log/cargued.log errors=3000
+sqlldr userid=daniel/123456 control=/home/oracle/database/oracle/ctl/cargued.ctl data=/home/oracle/database/oracle/csv/cargued.csv log=/home/oracle/database/oracle/log/cargued.log errors=3000
