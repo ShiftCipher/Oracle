45f5ff138d329f4e18f158a68b400be50ad3083b
codeapps <codeapps@aol.com>
2016-06-07 14:30:34 -0500

    Add Win Support for Loader

---
+++ loader.bat
@@ -0,0 +1,19 @@
+sqlldr userid=daniel/123456 control=X:\Apps\Editorial\Reference\Oracle\ctlwin\articulo.ctl data=X:\Apps\Editorial\Reference\Oracle\csv\articulo.csv log=X:\Apps\Editorial\Reference\Oracle\log\articulo.log errors=3000
+
+sqlldr userid=daniel/123456 control=X:\Apps\Editorial\Reference\Oracle\ctlwin\vendedor.ctl data=X:\Apps\Editorial\Reference\Oracle\csv\vendedor.csv log=X:\Apps\Editorial\Reference\Oracle\log\vendedor.log errors=3000
+
+sqlldr userid=daniel/123456 control=X:\Apps\Editorial\Reference\Oracle\ctlwin\cliente.ctl data=X:\Apps\Editorial\Reference\Oracle\csv\cliente.csv log=X:\Apps\Editorial\Reference\Oracle\log\cliente.log errors=3000
+
+sqlldr userid=daniel/123456 control=X:\Apps\Editorial\Reference\Oracle\ctlwin\conductor.ctl data=X:\Apps\Editorial\Reference\Oracle\csv\conductor.csv log=X:\Apps\Editorial\Reference\Oracle\log\conductor.log errors=3000
+
+sqlldr userid=daniel/123456 control=X:\Apps\Editorial\Reference\Oracle\ctlwin\ayudante.ctl data=X:\Apps\Editorial\Reference\Oracle\csv\ayudante.csv log=X:\Apps\Editorial\Reference\Oracle\log\ayudante.log errors=3000
+
+sqlldr userid=daniel/123456 control=X:\Apps\Editorial\Reference\Oracle\ctlwin\vehiculo.ctl data=X:\Apps\Editorial\Reference\Oracle\csv\vehiculo.csv log=X:\Apps\Editorial\Reference\Oracle\log\vehiculo.log errors=3000
+
+sqlldr userid=daniel/123456 control=X:\Apps\Editorial\Reference\Oracle\ctlwin\cabecera.ctl data=X:\Apps\Editorial\Reference\Oracle\csv\cabecera.csv log=X:\Apps\Editorial\Reference\Oracle\log\cabecera.log errors=3000
+
+sqlldr userid=daniel/123456 control=X:\Apps\Editorial\Reference\Oracle\ctlwin\detalle.ctl data=X:\Apps\Editorial\Reference\Oracle\csv\detalle.csv log=X:\Apps\Editorial\Reference\Oracle\log\detalle.log errors=3000
+
+sqlldr userid=daniel/123456 control=X:\Apps\Editorial\Reference\Oracle\ctlwin\carguec.ctl data=X:\Apps\Editorial\Reference\Oracle\csv\carguec.csv log=X:\Apps\Editorial\Reference\Oracle\log\carguec.log errors=3000
+
+sqlldr userid=daniel/123456 control=X:\Apps\Editorial\Reference\Oracle\ctlwin\cargued.ctl data=X:\Apps\Editorial\Reference\Oracle\csv\cargued.csv log=X:\Apps\Editorial\Reference\Oracle\log\cargued.log errors=3000
